0517dbe2a28937516e8232c2ba860473f80b6475
Junior Lima <juninho_livre2011@hotmail.com>
2023-02-08 22:09:24 -0300

    deploy

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'bf60ea46c2632ef8f5a6235807ac381b',
+  expires_in_token: '15m',
+  secret_refresh_token: 'bca6169d62b5fe62aa3cd1387b3b8117',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
